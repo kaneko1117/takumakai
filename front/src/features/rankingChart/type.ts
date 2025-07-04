78a8c8aa6e4b4e639b9085ee3dcf6e4ccd885f48
kaneko1117 <yuta_kaneko@pj100.biz>
2025-07-04 21:32:39 +0900

    不要なフォルダを削除

--- front/src/features/rankingChart/type.ts
+++
@@ -1,9 +0,0 @@
-type RankingChartSubject = "ラス回避率" | "連対率" | "トップ率";
-
-type InferredRankingChartType<T extends { subject: RankingChartSubject }> =
-  T extends { subject: infer U } ? U : never;
-
-export type RankingChartType = {
-  subject: RankingChartSubject;
-  percentage: number;
-};
